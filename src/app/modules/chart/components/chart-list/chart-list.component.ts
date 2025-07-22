e10018eebe736d67582a26e0cabb72a97fa98483
nfBarata <nfbarata@gmail.com>
2025-07-22 17:41:35 +0100

    charts select type

--- src/app/modules/chart/components/chart-list/chart-list.component.ts
+++ src/app/modules/chart/components/chart-list/chart-list.component.ts
@@ -13,26 +13,28 @@ import { Account } from '../../../../models/account';
 import { Transaction } from '../../../../models/transaction';
 import Chart, { ChartItem } from 'chart.js/auto'
 import { DineroPipe } from 'src/app/modules/shared/pipes/dinero.pipe';
-//import { AccountCreateCardComponent } from "../../../shared/components/account-create-card/account-create-card.component";
-//import { AccountSelectCardComponent } from "../../../shared/components/account-select-card/account-select-card.component";
-//import { TransactionCreateCardComponent } from "../../../shared/components/transaction-create-card/transaction-create-card.component";
-//import { TransactionsImportCardComponent } from "../../../shared/components/transactions-import-card/transactions-import-card.component";
+import {
+  UntypedFormBuilder,
+  UntypedFormControl,
+  UntypedFormGroup,
+  ReactiveFormsModule,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-chart-list',
   templateUrl: './chart-list.component.html',
-  styleUrls: ['./chart-list.component.css'],
-  //imports: [AccountCreateCardComponent, AccountSelectCardComponent, TransactionCreateCardComponent, TransactionsImportCardComponent],
+  styleUrls: ['./chart-list.component.css']
 })
 export class ChartListComponent implements OnInit, AfterViewInit {
   transactions: Transaction[] = [];
   selectedAccounts: Account[] = [];
   accounts: Account[] = [];
-  chartByEntity!: Chart<'pie', number[], string>;
-  chartByCategory!: Chart<'pie', number[], string>;
+  chart!: Chart<'doughnut', number[], string>;
 
-  @ViewChild('chartByEntity', { static: false }) chartByEntityReference!: ElementRef;
-  @ViewChild('chartByCategory', { static: false }) chartByCategoryReference!: ElementRef;
+  @ViewChild('chart', { static: false }) chartReference!: ElementRef;
+  form = new UntypedFormGroup({
+    groupBy: new UntypedFormControl(),
+  });
 
   constructor(
     private route: ActivatedRoute,
@@ -44,6 +46,10 @@ export class ChartListComponent implements OnInit, AfterViewInit {
   ) {}
 
   ngOnInit() {
+
+    this.form.setValue({
+      groupBy: 'entity'
+    });
 
     this.refreshAccounts();
 
@@ -70,10 +76,10 @@ export class ChartListComponent implements OnInit, AfterViewInit {
   }
 
   ngAfterViewInit(): void {
-    this.chartByEntity = new Chart(
-      this.chartByEntityReference.nativeElement,
+    this.chart = new Chart(
+      this.chartReference.nativeElement,
       {
-        type: 'pie',
+        type: 'doughnut',
         data: {
           labels: [],
           datasets: []
@@ -84,32 +90,10 @@ export class ChartListComponent implements OnInit, AfterViewInit {
             tooltip: {
               enabled: false
             },
-            title: {
+            /*title: {
               display: true,
-              text: 'Transactions by Entity' // i18n
-            }
-          }
-        }
-      }
-    );
-    this.chartByCategory = new Chart(
-      this.chartByCategoryReference.nativeElement,
-      {
-        type: 'pie',
-        data: {
-          labels: [],
-          datasets: []
-        },
-        options: {
-          responsive: true,
-          plugins: {
-            tooltip: {
-              enabled: false
-            },
-            title: {
-              display: true,
-              text: 'Transactions by Category' // i18n
-            }
+              text: 'Transactions' // i18n
+            }*/
           }
         }
       }
@@ -133,29 +117,34 @@ export class ChartListComponent implements OnInit, AfterViewInit {
       this.transactions = this.transactions.concat(account.transactions);
     });
 
-    if(this.chartByEntity!=null){
+    if(this.chart!=null){
       this.refreshChartData();
     }
   }
 
   refreshChartData(){
 
-    while(this.chartByEntity.data.labels!.length>0){
-      this.chartByEntity.data.labels!.pop();
+    while(this.chart.data.labels!.length>0){
+      this.chart.data.labels!.pop();
     }
-    while(this.chartByEntity.data.datasets.length>0) {
-      this.chartByEntity.data.datasets.pop();
+    while(this.chart.data.datasets.length>0) {
+      this.chart.data.datasets.pop();
     };
 
-    var transactionsByEntity = this.transactionService.groupByEntity(this.transactions);
+    var transactionsBy;
+    if(this.form.controls['groupBy'].value == 'entity'){
+      transactionsBy = this.transactionService.groupByEntity(this.transactions);
+    }else{
+      transactionsBy = this.transactionService.groupByCategory(this.transactions);
+    }
 
-    if(transactionsByEntity.size > 0) {
+    if(transactionsBy.size > 0) {
 
-      var usedCurrency = transactionsByEntity.values().next().value!.toJSON().currency;
+      var usedCurrency = transactionsBy.values().next().value!.toJSON().currency;
 
-      this.chartByEntity.data.labels! = Array.from(transactionsByEntity.keys());
-      this.chartByEntity.data.datasets.push({data: Array.from(transactionsByEntity.values()).map((d) => d.toJSON().amount)});
-      this.chartByEntity.options.plugins!.datalabels =  {
+      this.chart.data.labels! = Array.from(transactionsBy.keys());
+      this.chart.data.datasets.push({data: Array.from(transactionsBy.values()).map((d) => d.toJSON().amount)});
+      this.chart.options.plugins!.datalabels =  {
             formatter: (value, context) => {
               return this.dineroPipe.transform(MathService.toDinero(
                 parseFloat(value),
@@ -164,32 +153,6 @@ export class ChartListComponent implements OnInit, AfterViewInit {
             }
           };
     }
-    this.chartByEntity.update();
-
-    while(this.chartByCategory.data.labels!.length>0){
-      this.chartByCategory.data.labels!.pop();
-    }
-    while(this.chartByCategory.data.datasets.length>0) {
-      this.chartByCategory.data.datasets.pop();
-    };
-
-    var transactionsByCategory = this.transactionService.groupByCategory(this.transactions);
-
-    if(transactionsByCategory.size > 0) {
-
-      var usedCurrency = transactionsByCategory.values().next().value!.toJSON().currency;
-
-      this.chartByCategory.data.labels! = Array.from(transactionsByCategory.keys());
-      this.chartByCategory.data.datasets.push({data: Array.from(transactionsByCategory.values()).map((d) => d.toJSON().amount)});
-      this.chartByCategory.options.plugins!.datalabels =  {
-            formatter: (value, context) => {
-              return this.dineroPipe.transform(MathService.toDinero(
-                parseFloat(value),
-                usedCurrency
-              ));
-            }
-          };
-    }
-    this.chartByCategory.update();
+    this.chart.update();
   }
 }
